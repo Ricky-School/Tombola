768b13ea1577e93b549d95ff9411368ad03fef22
Rick Monta <sriccardo.montanini@itis.pr.it>
2024-01-22 18:30:31 +0100

    Prova

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+let righe = 10;
+let colonne = 10;
+let numeriEstratti = [];
+
+function DisegnaTabella() {
+    let div1 = document.getElementById('div1');
+    let tabella = document.createElement("table");
+    var i = 1;
+    for (var r = 0; r < righe; r++) {
+        let row = document.createElement("tr");
+        for (var c = 0; c < colonne; c++, i++) {
+            let cell = document.createElement("td");
+            let cellText = document.createTextNode(i);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+            if (numeriEstratti.includes(i)) cell.style.backgroundColor = 'red';
+        }
+        tabella.appendChild(row);
+    }
+
+    div1.appendChild(tabella);
+}
+
+function estraiNumero() {
+    var num = Math.floor(Math.random() * 100 + 1);
+    while(true) {
+        if (!numeriEstratti.includes(num)){
+            break;
+        }
+        num = Math.floor(Math.random() * 100 + 1);
+    }
+    
+    let div1 = document.getElementById('div1');    
+    while (div1.firstChild) {
+        div1.removeChild(div1.firstChild);
+    }
+    numeriEstratti.push(num);
+    DisegnaTabella();
+}
+
+DisegnaTabella();
